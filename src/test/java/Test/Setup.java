153a810674b7e32d9268de19a5195debb8340451
omarsalas94 <omar-salas@outlook.com>
2017-08-03 13:17:54 -0500

    Update Setup.java

--- src/test/java/Test/Setup.java
+++ src/test/java/Test/Setup.java
@@ -1,8 +1,14 @@
+
 package Test;
 
 public class Setup {
 
     public Setup(){
         System.out.println("Hola pancho");
+        System.out.println("Otro hola mundo");
+    }
+    
+    public void prueba(){
+        System.out.println("Kiubo Pancho Budud");
     }
 }
