fab82be4fe69e5fbdde0f6f968fd1f8f3fe33245
Francisco Bujdud <fxbujdud@gmail.com>
2017-08-03 13:22:21 -0500

    Prueba cambios

--- src/test/java/Test/Setup.java
+++ src/test/java/Test/Setup.java
@@ -4,15 +4,7 @@ package Test;
 public class Setup {
 
     public Setup(){
-        System.out.println("Hola pancho");
-        System.out.println("Otro hola mundo");
+
     }
     
-    public void prueba(){
-        System.out.println("Kiubo Pancho Budud");
-        System.out.println("Kiubo Pancho Budud");
-        System.out.println("Kiubo Pancho Budud");
-        System.out.println("Kiubo Pancho Budud");
-        System.out.println("Kiubo Pancho Budud");
-    }
 }
